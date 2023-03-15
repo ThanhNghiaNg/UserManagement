3d094b78d48ad6b5892c35b7705be8da0e6a80e5
ThanhNghiaNg <nghiant089@gmail.com>
2023-03-15 13:18:30 +0700

    rename backend to server

---
+++ server/index.js
@@ -0,0 +1,33 @@
+const express = require("express");
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
+const mongoose = require("mongoose");
+const cookieParser = require("cookie-parser");
+const authRoute = require("./routes/auth.js");
+const userRoute = require("./routes/user.js");
+dotenv.config();
+
+mongoose.connect(process.env.MONGODB_URL, () => {
+  console.log("CONNECTED TO MONGO DB");
+});
+app.use(
+  cors({
+    origin: true,
+    credentials: true,
+  })
+);
+app.use(cookieParser());
+app.use(express.json());
+
+//ROUTES
+app.use("/v1/auth", authRoute);
+app.use("/v1/user", userRoute);
+
+app.get("/test", (req, res, next) => {
+  res.status(200).json({ success: "Hello Server" });
+});
+
+app.listen(process.env.PORT, () => {
+  console.log("Server is running");
+});
